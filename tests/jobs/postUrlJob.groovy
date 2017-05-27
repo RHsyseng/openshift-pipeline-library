522b5060456cda5fd8e0493d1b6fc0d5ec495bcf
Joseph Callen <jcallen@redhat.com>
2017-05-27 09:42:32 -0400

    testing var with jsonbuilder

--- tests/jobs/postUrlJob.groovy
+++ tests/jobs/postUrlJob.groovy
@@ -6,30 +6,10 @@ import com.redhat.*
 
 node {
 
-    def utils = new Utils()
-
-    stage('test') {
-
-        String jsonString = """
-        {
-            "secret": "${secret}",
-            "pid": "p17633880910e488f5949aab3ad76cd4317542a7a06"
-        }
-        """
-
-        def json = new groovy.json.JsonBuilder()
-
-        def root = json secret: secret, pid: "p17633880910e488f5949aab3ad76cd4317542a7a06"
-
-        println(root.toString())
-
-        def results = utils.postUrl("https://connect.redhat.com/api/container/status",
-                root.toString(), true)
-
-
-        println(results)
+    rebuildImage{
+        pid = "p17633880910e488f5949aab3ad76cd4317542a7a06"
+        secret = "${secret}"
     }
-
 
 
 }
---
+++ vars/rebuildImage.groovy
@@ -0,0 +1,24 @@
+#!groovy
+
+@Library('Utils')
+import com.redhat.*
+
+def call(Closure body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    String uri = "https://connect.redhat.com/api/container/status"
+    def utils = new Utils()
+
+    stage('Check Container Status') {
+
+        def json = new groovy.json.JsonBuilder()
+
+        def root = json secret: config['secret'], pid: config['pid']
+
+        def results = utils.postUrl(uri, root.toString(), true)
+        println(results)
+    }
+}
